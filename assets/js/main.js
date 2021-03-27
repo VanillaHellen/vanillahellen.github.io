1e4b486cc2f05aa0d877c3908ced2399ca16ea7b
VanillaHellen <vanillahellen@gmail.com>
2021-03-27 19:34:39 +0100

    final look changes + partly cleaned the css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,4 @@
-(function(){
-  // Vertical Timeline - by CodyHouse.co
-	function VerticalTimeline( element ) {
-		this.element = element;
-		this.blocks = this.element.getElementsByClassName("cd-timeline__block");
-		this.images = this.element.getElementsByClassName("cd-timeline__img");
-		this.contents = this.element.getElementsByClassName("cd-timeline__content");
-		this.offset = 0.8;
-	};
-
-	var verticalTimelines = document.getElementsByClassName("js-cd-timeline"),
-		verticalTimelinesArray = [],
-		scrolling = false;
-	if( verticalTimelines.length > 0 ) {
-		for( var i = 0; i < verticalTimelines.length; i++) {
-			(function(i){
-				verticalTimelinesArray.push(new VerticalTimeline(verticalTimelines[i]));
-			})(i);
-		}
-
-		//show timeline blocks on scrolling
-		window.addEventListener("scroll", function(event) {
-			if( !scrolling ) {
-				scrolling = true;
-				(!window.requestAnimationFrame) ? setTimeout(checkTimelineScroll, 250) : window.requestAnimationFrame(checkTimelineScroll);
-			}
-		});
-	}
-
-	function checkTimelineScroll() {
-		verticalTimelinesArray.forEach(function(timeline){
-		});
-		scrolling = false;
-	};
-
+$(function(){
 	$( ".history-btn").click(function() {
 		$(this).parent().parent().find( ".attack" ).slideUp( "fast" );
 		$(this).parent().parent().find( ".protect" ).slideUp( "fast" );
@@ -50,4 +16,4 @@
 		$(this).parent().parent().find( ".attack" ).slideUp( "fast" );
 		$(this).parent().parent().find( ".protect" ).slideToggle( "fast" );
 	});
-})();
+});
