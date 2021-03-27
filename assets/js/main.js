17fb8459a5867be8633a6f649c2bbcb415d5f315
VanillaHellen <vanillahellen@gmail.com>
2021-03-27 18:40:53 +0100

    fix buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,8 +29,25 @@
 
 	function checkTimelineScroll() {
 		verticalTimelinesArray.forEach(function(timeline){
-			timeline.showBlocks();
 		});
 		scrolling = false;
 	};
+
+	$( ".history-btn").click(function() {
+		$(this).parent().parent().find( ".attack" ).slideUp( "fast" );
+		$(this).parent().parent().find( ".protect" ).slideUp( "fast" );
+		$(this).parent().parent().find( ".history" ).slideToggle( "fast" );
+	});
+
+	$( ".attack-btn").click(function() {
+		$(this).parent().parent().find( ".history" ).slideUp( "fast" );
+		$(this).parent().parent().find( ".protect" ).slideUp( "fast" );
+		$(this).parent().parent().find( ".attack" ).slideToggle( "fast" );
+	});
+
+	$( ".protect-btn").click(function() {
+		$(this).parent().parent().find( ".history" ).slideUp( "fast" );
+		$(this).parent().parent().find( ".attack" ).slideUp( "fast" );
+		$(this).parent().parent().find( ".protect" ).slideToggle( "fast" );
+	});
 })();
